879fa8685c11ad31dce012c942ed1fce34c60a51
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-20 13:13:20 -0500

    commit tree 39759fc07eb19ad2488b30544aba60d5edc962b5

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        self.roster[grade] ||= []
+        self.roster[grade] << student_name 
+    end
+   
+    def grade(student_grade)
+        self.roster[student_grade]
+    end
+
+    def sort
+        sorted = {}
+        self.roster.each do |grade, students| 
+            sorted[grade] = students.sort
+        end
+        sorted
+    end
+
+
+end
